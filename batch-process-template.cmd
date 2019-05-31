5e611c7aa40e2df55068b469a6f3e935746ca78e
William Winslow <thesythel@gmail.com>
2019-05-31 11:44:50 -0400

    fix for batch-process-template

--- batch-process-template.cmd
+++ batch-process-template.cmd
@@ -2,30 +2,30 @@ SPECIAL>NOINTERACTION
 SPECIAL>LOADMODEL|"E:\fish-energy-tradeoff-vensim\energy-fish-cost tradeoffs-042819.vmf"
 
 
-SIMULATE>SETVAL|initial dam1 install Pool and Weir=${field1}
-SIMULATE>SETVAL|initial dam1 install Denil=${field2}
-SIMULATE>SETVAL|initial dam1 install Fish Lift=${field3}
-SIMULATE>SETVAL|initial remove dam1=${field4}
+SIMULATE>SETVAL|dam1 install Pool and Weir=${field1}
+SIMULATE>SETVAL|dam1 install Denil=${field2}
+SIMULATE>SETVAL|dam1 install Fish Lift=${field3}
+SIMULATE>SETVAL|remove dam1=${field4}
 
-SIMULATE>SETVAL|initial dam2 install Pool and Weir=${field5}
-SIMULATE>SETVAL|initial dam2 install Denil=${field6}
-SIMULATE>SETVAL|initial dam2 install Fish Lift=${field7}
-SIMULATE>SETVAL|initial remove dam2=${field8}
+SIMULATE>SETVAL|dam2 install Pool and Weir=${field5}
+SIMULATE>SETVAL|dam2 install Denil=${field6}
+SIMULATE>SETVAL|dam2 install Fish Lift=${field7}
+SIMULATE>SETVAL|remove dam2=${field8}
 
-SIMULATE>SETVAL|initial dam3 install Pool and Weir=${field9}
-SIMULATE>SETVAL|initial dam3 install Denil=${field10}
-SIMULATE>SETVAL|initial dam3 install Fish Lift=${field11}
-SIMULATE>SETVAL|initial remove dam3=${field12}
+SIMULATE>SETVAL|dam3 install Pool and Weir=${field9}
+SIMULATE>SETVAL|dam3 install Denil=${field10}
+SIMULATE>SETVAL|dam3 install Fish Lift=${field11}
+SIMULATE>SETVAL|remove dam3=${field12}
 
-SIMULATE>SETVAL|initial dam4 install Pool and Weir=${field13}
-SIMULATE>SETVAL|initial dam4 install Denil=${field14}
-SIMULATE>SETVAL|initial dam4 install Fish Lift=${field15}
-SIMULATE>SETVAL|initial remove dam4=${field16}
+SIMULATE>SETVAL|dam4 install Pool and Weir=${field13}
+SIMULATE>SETVAL|dam4 install Denil=${field14}
+SIMULATE>SETVAL|dam4 install Fish Lift=${field15}
+SIMULATE>SETVAL|remove dam4=${field16}
 
-SIMULATE>SETVAL|initial dam5 install Pool and Weir=${field17}
-SIMULATE>SETVAL|initial dam5 install Denil=${field18}
-SIMULATE>SETVAL|initial dam5 install Fish Lift=${field19}
-SIMULATE>SETVAL|initial remove dam5=${field20}
+SIMULATE>SETVAL|dam5 install Pool and Weir=${field17}
+SIMULATE>SETVAL|dam5 install Denil=${field18}
+SIMULATE>SETVAL|dam5 install Fish Lift=${field19}
+SIMULATE>SETVAL|remove dam5=${field20}
 
 
 SIMULATE>RUNNAME|fish-energy-run-${RunNumber}
